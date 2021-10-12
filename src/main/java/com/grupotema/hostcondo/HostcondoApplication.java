78144802b2cb35e390973989c699119086c69b87
Marlon Prado <marlonprado.cn@gmail.com>
2021-10-12 20:25:19 -0300

    Projeto criado

---
+++ src/main/java/com/grupotema/hostcondo/HostcondoApplication.java
@@ -0,0 +1,13 @@
+package com.grupotema.hostcondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostcondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostcondoApplication.class, args);
+	}
+
+}
